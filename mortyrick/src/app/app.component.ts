95195b22582403a1bda47b19bf213e2bb0ef2c62
DrabantBY <drabant2022@gmail.com>
2023-12-23 02:11:39 +0100

    add navbar

--- mortyrick/src/app/app.component.ts
+++ mortyrick/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
