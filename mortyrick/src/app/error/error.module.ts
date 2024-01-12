d847fff5cb9a33bcd4558551bcad4cbd0f31d0dc
DrabantBY <drabant2022@gmail.com>
2024-01-13 00:50:48 +0100

    add styles for base components

--- mortyrick/src/app/error/error.module.ts
+++ mortyrick/src/app/error/error.module.ts
@@ -1,15 +1,26 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { ErrorComponent } from './components/error/error.component';
+
 import { GlobalClientErrorService } from './services/global-client-error.service';
 import { GlobalServerError } from './services/global-server-error.service';
 
 const routes = [{ path: 'error', component: ErrorComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   declarations: [ErrorComponent],
   providers: [
     { provide: ErrorHandler, useClass: GlobalClientErrorService },
